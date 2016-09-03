7e87b53ba668f8f1f2d5b2bfb0747405474e3342
Jason Wu <kungpowjason@gmail.com>
2016-09-03 11:58:37 -0400

    first commit

---
+++ CS4341_Assignment_1.py
@@ -0,0 +1,8 @@
+#CS4341 Assignment 1
+search_type = input('Search type? (iterative, greedy): ')
+starting_value = input('Starting value?: ')
+target_value = input('Target_Value?: ')
+time_limit = input('Time limit? (seconds): ')
+operations = input('Operations? (separate by commas): ')
+
+print search_type +'/n'+starting_value+'/n'+target_value+'/n'+time_limit+'/n'+operations
