f02f7bb09bb5b27ea07816a666908dd1471ff886
kartikvasu <kartiktv97@gmail.com>
2016-09-13 16:53:37 -0400

    Adding new directory structure

--- Assignment_1_wu_graff_vasu/CS4341_Assignment_1.py
+++ Assignment_1_wu_graff_vasu/CS4341_Assignment_1.py
@@ -7,6 +7,8 @@ import sys
 import time
 import itertools
 
+#Instead of using numpy arithmetic functions, just made some functions to use
+
 def summation(a,b):
 	return a+b
 
@@ -22,6 +24,8 @@ def quotient(a,b):
 def power(a,b):
 	return a**b
 
+#Here is our function switch case
+
 math_func = {'+' : summation,
 		   '-' : difference,
 		   '*' : multiply,
@@ -29,20 +33,22 @@ math_func = {'+' : summation,
 		   '^' : power,
 }
 
+#Algorithm Search Class for each type
 class SearchAlgorithm:
-	def __init__(self, start, goal, operations_list, solution_path):
+	def __init__(self, start, goal, operations_list):
 		self.start_node = Node(None, None, int(start))
 		self.current_node = self.start_node
 		self.goal = goal
 		self.operations_list = operations_list
-		self.num_nodesexpanded = 0
+		self.num_nodesexpanded = 1
 		self.OPEN = []
 		self.CLOSED = []
 		self.depth = 0
 		self.max_depth = 0
 		self.best_node = self.current_node
-		self.solution_path = solution_path
+		self.solution_path = []
 
+	#Normal depth limited; returns True is found
 	def dl_search(self, node, depth):
 		if (depth == 0) and (node.value == self.goal):
 			return True
@@ -50,90 +56,81 @@ class SearchAlgorithm:
 			return True
 		elif (depth > 0):
 			node.createChildren(self.operations_list)
+			self.num_nodesexpanded += len(self.operations_list)
 			for child in node.children:
-				self.num_nodesexpanded += 1
 				self.current_node = child
 				if (abs(self.current_node.value - self.goal) < abs(self.best_node.value - self.goal)):
 					self.best_node = self.current_node
+				if (child.depth > self.max_depth):
+					self.max_depth = child.depth
 				#print (child.value)
 				found = self.dl_search(child, depth-1)
 				if found:
 					return True
 		return False
-
+	#Iterative Deepening using depth first increasing depth each time
 	def id_search(self):
 		depth = 0
 		#print ('GOT HERE')
 		while self.current_node.value != self.goal:
 			#print (depth)
 			if self.dl_search(self.start_node, depth):
-				self.best_node = self.current_node
 				return self.current_node
 			else:
 				depth += 1
-				if depth > self.max_depth:
-					self.max_depth = depth
 
+	#Greedy Best First Search using OPEN and CLOSED node lists
 	def gbf_search(self):
 		while(self.current_node.value != self.goal):
 			self.current_node.createChildren(self.operations_list)
+			self.num_nodesexpanded += len(self.operations_list)
 			if (abs(self.current_node.value - self.goal) < abs(self.best_node.value - self.goal)):
 				self.best_node = self.current_node
-			self.num_nodesexpanded += 1
 			self.CLOSED.append(self.current_node)
 			for child in self.current_node.children:
+				if (child.depth > self.max_depth):
+					self.max_depth = child.depth
 				if(child not in self.OPEN) and (child not in self.CLOSED):
 					self.OPEN.append(child)
 					self.reorderOpen()
 			self.current_node = self.OPEN.pop(0)
 		self.best_node = self.current_node
-		return self.current_node
+		return self.best_node
 
+	#Reorders the open list according to our heuristic function
 	def reorderOpen(self):
-		new_list = []
-		for node in self.OPEN:
-			if not new_list:
-				new_list.append(node)
-			else:	
-				for item in new_list:
-					h1 = abs(self.goal - node.value)
-					h2 = abs(self.goal - item.value)
-					if(h1 < h2):
-						new_list.append(node)
-						break
-		self.OPEN = new_list
+		self.OPEN.sort(key=lambda x: abs(self.goal - x.value))
 
-	def findMaxDepth(self):
-		for node in self.CLOSED:
-				self.solution_path = []
-				node.backtrackNode(self.solution_path)
-				if self.solution_path:
-					current_depth = len(self.solution_path)+1 #total number of parents for selected node
-					if(self.max_depth < current_depth):
-						self.max_depth = current_depth
-		#print (self.max_depth)
-		return self.max_depth
-
+#Operation class holding an operator and integer from file input
 class Operation:
 	def __init__(self, operator, integer):
 		self.operator = operator
 		self.integer = integer
 
+#Basic Node class that contains info about its value, the operation it took to get there, and its parents/children
 class Node:
 	def __init__(self, parent, operation, value):
 		self.parent = parent
 		self.operation = operation
 		self.value = value
 		self.children = []
+		#keep track of depth
+		if(parent is None):
+			self.depth = 0
+		else:
+			self.depth = parent.depth + 1
 
+	#Create the children nodes
 	def createChildren(self, operations_list):
 		if not self.children:
 			for operation in operations_list:
 				self.children.append(Node(self,operation, self.evalChildValue(operation)))
 
+	#Evaluates the value of the child
 	def evalChildValue(self, operation):
 		return math_func[operation.operator](self.value, operation.integer)
 
+	#Traverses the ancestry of a particular node and returns a list of nodes (ancestry)
 	def backtrackNode(self, solution_path):
 		if (self.parent is None):
 			return solution_path.reverse()
@@ -142,17 +139,18 @@ class Node:
 			solution_path.append(self.parent)
 			self.parent.backtrackNode(solution_path)
 
-	def backtrackNode2(self, solution_path):
+	#Prints out the operations that it took to reach the solution
+	def printSolution(self, solution_path):
 		if (self.parent is None):
-			return solution_path.reverse()
+			return
 		if not (self.parent is None):
 			print (str(self.parent.value) + ' ' + self.operation.operator + ' ' + str(self.operation.integer) + ' = ' + str(self.value))
 			solution_path.append(self.parent)
-			self.parent.backtrackNode2(solution_path)
+			self.parent.printSolution(solution_path)
 
-
-
-def parse_operations(strlist):
+#Breaks up a string into parameters to create an Operations object assuming first value is the operation
+#and the rest is the integer
+def parseOperations(strlist):
 	string_arr = strlist
 	operations_list = []
 	for string in string_arr:
@@ -160,9 +158,19 @@ def parse_operations(strlist):
 		operations_list.append(Operation(letter_arr[0],int(''.join(letter_arr[1:]))))
 	return operations_list
 
+#Prints the stats
+def printStats(search_type, error, steps, time, nodes_expanded, max_depth):
+	print ('\n\n' + search_type)
+	print ('error: '+ error)
+	print ('Number of steps required: ' + steps)
+	print ('Search time required: ' + time + ' seconds')
+	print ('Nodes expanded: ' + nodes_expanded)
+	print ('Maximum depth: ' + max_depth)
+
 
 class TimeoutException(Exception): pass
 
+# Handles our time out, raise the exception and does something about it
 @contextmanager
 def time_limit_manager(seconds):
 	def signal_handler(signum, frame):
@@ -184,6 +192,7 @@ iterative_results = [[0 for x in range(w)] for y in range(h)]
 greedy_results = [[0 for x in range(w)] for y in range(h)] 
 #the zero these place in the first index must be accounted for in the avg
 
+#Limit the recursion limit
 sys.setrecursionlimit(10000)
 for filename in _iterArg:
 	if len(argv) > 1 :
@@ -205,40 +214,34 @@ for filename in _iterArg:
 			print ("not enough arguments in file")
 			exit(0)
 
-	else:
-		search_type = input('Search type? (iterative, greedy): ')
-		starting_value = float(input('Starting value?: '))
-		target_value = float(input('Target_Value?: '))
-		time_limit = float(input('Time limit? (seconds): '))
-		operations = input('Operations? (separate by spaces): ')
-		operations_parsed = parse_operations(operations)
+	# else:
+	# 	# Manual Input
+	# 	search_type = input('Search type? (iterative, greedy): ')
+	# 	starting_value = float(input('Starting value?: '))
+	# 	target_value = float(input('Target_Value?: '))
+	# 	time_limit = float(input('Time limit? (seconds): '))
+	# 	operations = input('Operations? (separate by spaces): ')
+	# 	operations_parsed = parseOperations(operations)
 
-
-	# print (search_type +'\n'+starting_value+'\n'+target_value+'\n'+time_limit+'\n'+ operations_parsed[0]+'\n'+operations_parsed[1]+'\n'+operations_parsed[2])
+	
 	try:
 		with time_limit_manager(int(time_limit)):
+
 			start_time = time.time()
-			solution_path = []
-			id = SearchAlgorithm(int(starting_value), int(target_value), operations_parsed, solution_path)
-			id.num_nodesexpanded = 0
-			id.depth = 0
+			id = SearchAlgorithm(int(starting_value), int(target_value), operations_parsed)
 
-
+			# ERIK IS OUR SOLUTION NODE
 			if (search_type == 'iterative'):
 				erik = id.id_search()
 			elif (search_type == 'greedy'):
 				erik = id.gbf_search()
 			end_time = time.time()
-			print ('\nDONE')
-			erik.backtrackNode2(id.solution_path)
+			print ('\nFOUND SOLUTION')
+
+			erik.printSolution(id.solution_path)
 			execution_time = str(end_time - start_time)
-			curr_max_depth =id.findMaxDepth()
-			print ('\n\n' + search_type)
-			print ('error: '+ str(abs(id.best_node.value - target_value)))
-			print ('Number of steps required: ' + str(len(id.solution_path)))
-			print ('Search required: ' + execution_time + ' seconds')
-			print ('Nodes expanded: ' + str(id.num_nodesexpanded))
-			print ('Maximum depth: ' + str(curr_max_depth))
+			printStats(search_type, str(abs(id.best_node.value - target_value)),str(len(id.solution_path)),
+				execution_time, str(id.num_nodesexpanded), str(id.max_depth))
 
 			if (search_type == 'iterative'):
 				iterative_results[0].append(float(execution_time)) #store execution time
@@ -247,44 +250,16 @@ for filename in _iterArg:
 			elif (search_type == 'greedy'):
 				greedy_results[0].append(float(execution_time)) #store execution time
 				greedy_results[1].append(id.num_nodesexpanded)#store num expanded
-				greedy_results[2].append(curr_max_depth)#store maximum depth
+				greedy_results[2].append(id.max_depth)#store maximum depth
 	except (TimeoutException, RuntimeError) as error:
 		end_time = time.time()
-		print ('Timed Out: ' + str(error.args))
+		print ('Could not find solution\nTimed Out: ' + str(error.args))
 		if (search_type == 'iterative'):
 				iterative_results[3][0] = iterative_results[3][0]+1
-				id.best_node.backtrackNode2(id.solution_path)
+				id.best_node.printSolution(id.solution_path)
 		elif (search_type == 'greedy'):
 				greedy_results[3][0] = greedy_results[3][0]+1
-				id.best_node.backtrackNode2(id.solution_path)
+				id.best_node.printSolution(id.solution_path)
 		execution_time = str(end_time - start_time)
-		curr_max_depth =id.findMaxDepth()
-		print ('\n\n' + search_type)
-		print ('error: '+ str(abs(id.best_node.value - target_value)))
-		print ('Number of steps required: ' + str(len(id.solution_path)))
-		print ('Search required: ' + execution_time + ' seconds')
-		print ('Nodes expanded: ' + str(id.num_nodesexpanded))
-		print ('Maximum depth: ' + str(curr_max_depth))
-	
-
-
-if len(argv) > 1:
-	#Average and print cumulative results
-	for result in iterative_results:
-		sigma = float(0)
-		for value in result:
-			sigma = sigma + value
-		if len(result) > 1:
-			result[0] = sigma / (len(result)-1) #average the results and store in index 0 (-1 accounts for preceeding 0)
-			#iterative_results[n][x] retains the initial result as well. There happens to be a blank start.
-	for result in greedy_results:
-		sigma = float(0)
-		for value in result:
-			sigma = sigma + value
-		if len(result) > 1:
-			result[0] = sigma / (len(result)-1) #average the results and store in index 0 (-1 accounts for preceeding 0)
-
-	print ('\nAverage execution time '+ 'Iterative: '+str(iterative_results[0][0])+', Greedy: '+str(greedy_results[0][0]))
-	print ('Average nodes expanded '+ 'Iterative: '+str(iterative_results[1][0])+', Greedy: '+str(greedy_results[1][0]))
-	print ('Average solution length '+ 'Iterative: '+str(iterative_results[2][0])+', Greedy: '+str(greedy_results[2][0]))
-	print ('Number timed out  '+ 'Iterative: '+str(iterative_results[3][0])+', Greedy: '+str(greedy_results[3][0]))
+		printStats(search_type, str(abs(id.best_node.value - target_value)),str(len(id.solution_path)),
+				execution_time, str(id.num_nodesexpanded), str(id.max_depth))
