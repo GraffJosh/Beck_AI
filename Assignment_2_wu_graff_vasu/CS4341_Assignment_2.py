0fee4e58ce3ebafa1e65bf390124552c29a9d9f4
kartikvasu <kartiktv97@gmail.com>
2016-09-14 19:25:52 -0400

    Works, still have to implement culling. Weighted probabilities work.

--- Assignment_2_wu_graff_vasu/CS4341_Assignment_2.py
+++ Assignment_2_wu_graff_vasu/CS4341_Assignment_2.py
@@ -1,4 +1,4 @@
-#CS4341 Assignment 1
+#CS4341 Assignment 2
 from __future__ import with_statement # Required in 2.5
 import signal
 from contextlib import contextmanager
@@ -7,6 +7,8 @@ import sys
 import time
 import itertools
 import random
+import numpy
+import math
 
 #Instead of using numpy arithmetic functions, just made some functions to use
 
@@ -39,38 +41,93 @@ class SearchAlgorithm:
 	def __init__(self, start, goal, operations_list):
 		#self.start_node = Node(None, None, int(start))
 		#self.current_node = self.start_node
+		self.start = start
 		self.goal = goal
 		self.operations_list = operations_list
 		self.num_nodesexpanded = 1
 		self.depth = 0
 		#self.best_node = self.current_node
+
+		# A zoo is an array of "nodes" where each node contains 
+		# a list of operators.
 		self.zoo = []
-
-
 
 	#Reorders the open list according to our heuristic function
 	def reorderOpen(self):
 		self.OPEN.sort(key=lambda x: abs(self.goal - x.value))
 
-
 	def init_operations(self):
-		num_nodes = 50 			#number of nodes in a zoo
-		num_operations = 30		#number of operators per node
-		op_array = []
-		for node_num in range(num_nodes):
-			#appends to the zoo an initialized node.
-			for op_num in range(num_operations):
-				#appends to op_array a random operator from our pool
-				random.shuffle(self.operations_list)
-				op_array.append(self.operations_list[1])
-			self.zoo.append(Node(op_array))
+		num_nodes = 50 			# number of nodes in a zoo
+		num_operations = 10		# number of operators per node
+		
 
+		for node_num in range(num_nodes):
+
+			op_array = [] # a list of nodes
+			
+			# appends to the zoo an initialized node.
+			for op_num in range(num_operations):
+				# appends to op_array a random operator from our pool
+				op_array.append(random.choice(self.operations_list))
+
+			self.zoo.append(Node(self.start, self.goal, op_array))
 
 	def genetic_search(self):
 		self.init_operations()
-		#holy shit do we need a better way to notate this.
-		print (self.zoo[1].operations[1].operator)
+		num_reproductions = 10
 
+		for num in range(num_reproductions):
+			self.zoo = self.genetic_search_recurse(self.zoo)
+
+		best_organism = self.zoo[0]
+
+		for organism in self.zoo:
+			if organism.eval_node_fitness() == float("inf"):
+				best_organism = organism
+
+			if organism.eval_node_fitness() > best_organism.eval_node_fitness():
+				best_organism = organism
+
+		#best_organism.printSolution()
+		#print(best_organism.eval_node_fitness())
+
+		return best_organism
+
+	def genetic_search_recurse(self, zoo):
+
+		new_zoo = [] # this is the new population
+		for organism in self.zoo:
+
+			# these two operations should happen based on the fitness function
+			parentA = self.choose_weighted_parent(self.zoo) 
+			parentB = self.choose_weighted_parent(self.zoo)
+
+			child = self.reproduce(parentA, parentB)
+			new_zoo.append(child)
+
+		return new_zoo
+
+	def reproduce(self, orgA, orgB):
+		length = len(orgA.operations)
+		cut_off = random.randint(0, length)
+
+		child_operations = orgA.operations[:cut_off] + orgB.operations[cut_off:]
+		child = Node(orgA.start, orgB.goal, child_operations)
+		return child
+
+	def choose_weighted_parent(self, zoo):
+		fitnesses = []
+		weights = []
+		sum = 0
+
+		for organism in zoo:
+			sum = sum + organism.eval_node_fitness()
+			fitnesses.append(organism.eval_node_fitness())
+
+		for fitness in fitnesses:
+			weights.append(fitness/sum)
+
+		return numpy.random.choice(zoo, p = weights)
 
 #Operation class holding an operator and integer from file input
 class Operation:
@@ -78,36 +135,38 @@ class Operation:
 		self.operator = operator
 		self.integer = integer
 
-#Basic Node class that contains info about its value, the operation it took to get there, and its parents/children
+# For the genetic algorithm, a node contains information about
+# the operators that are being evaluated.  
 class Node:
-		#determine the node's value
-
+		
 	def eval_node_val(self):
 		return 1
 
-	def eval_node_h(self):
-		return 1
+	def eval_node_fitness(self):
+		num = self.start
 
-	def __init__(self, operations):
-		self.value = self.eval_node_val()
+		for operation in self.operations:
+			num = math_func[operation.operator](num, operation.integer)
+		
+		if abs(self.goal - num) == 0:
+			return float("inf")
+
+		return 1 / abs(self.goal - num)
+
+	def __init__(self, start_value, target_value, operations):
+		#self.value = self.eval_node_val()
 		self.operations = operations
-		self.heuristic = self.eval_node_h()
+		self.start = start_value
+		self.goal = target_value
+		#self.heuristic = self.eval_node_h()
 
+	def printSolution(self):
+		num = self.start
 
-
-
-
-
-
-
-	#Prints out the operations that it took to reach the solution
-	def printSolution(self, solution_path):
-		if (self.parent is None):
-			return
-		if not (self.parent is None):
-			print (str(self.parent.value) + ' ' + self.operation.operator + ' ' + str(self.operation.integer) + ' = ' + str(self.value))
-			solution_path.append(self.parent)
-			self.parent.printSolution(solution_path)
+		for operation in self.operations:
+			next_num = math_func[operation.operator](num, operation.integer)
+			print (str(num) + ' ' + operation.operator + ' ' + str(operation.integer) + ' = ' + str(next_num))
+			num = next_num
 
 #Breaks up a string into parameters to create an Operations object assuming first value is the operation
 #and the rest is the integer
@@ -194,17 +253,17 @@ for filename in _iterArg:
 			if (search_type == 'genetic'):
 				erik = id.genetic_search()
 			end_time = time.time()
-			print ('\nFOUND SOLUTION')
+			print ('\nCLOSEST SOLUTION PATH')
 
-			erik.printSolution(id.solution_path)
+			erik.printSolution()
 			execution_time = str(end_time - start_time)
-			printStats(search_type, str(abs(id.best_node.value - target_value)),str(len(id.solution_path)),
-			execution_time, str(id.num_nodesexpanded), str(id.max_depth))
+			#printStats(search_type, str(abs(id.best_node.value - target_value)),str(len(id.solution_path)),
+			#execution_time, str(id.num_nodesexpanded), str(id.max_depth))
 
 			if (search_type == 'genetic'):
 				genetic_results[0].append(float(execution_time)) #store execution time
-				genetic_results[1].append(id.num_nodesexpanded)#store num expanded
-				genetic_results[2].append(id.max_depth)#store maximum depth
+				#genetic_results[1].append(id.num_nodesexpanded)#store num expanded
+				#genetic_results[2].append(id.max_depth)#store maximum depth
 
 	except (TimeoutException, RuntimeError) as error:
 		end_time = time.time()
